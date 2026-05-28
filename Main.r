0b4e400d74eee285a9a3546d9b09b1ef0b9b93b8
Alessandro <124038277+AlessandroTbz@users.noreply.github.com>
2026-05-28 13:27:21 +0000

    first commit from codespace

---
+++ Main.r
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+source("R/setup.r")
+source("R/census.r")
+source("R/mortality.r")
+source("R/merge.Rr)
+
+head(dataset_finale)
---
+++ R/setpu.r
@@ -0,0 +1,8 @@
+library(tidycensus)
+library(tidyverse)
+library(readr)
+
+readRenviron("~/.Renviron")
+options(timeout = 300)
+
+anno_studio <- 2019
